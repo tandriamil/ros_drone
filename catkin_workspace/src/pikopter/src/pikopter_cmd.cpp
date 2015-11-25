f91ad81dc719718f1c4ab4a708388800fc845943
Yann JEGU <y.jegu@hotmail.fr>
2015-11-25 18:03:56 +0100

    Finishing communication between command sent and node pikopter_cmd

--- catkin_workspace/src/pikopter/src/pikopter_cmd.cpp
+++ catkin_workspace/src/pikopter/src/pikopter_cmd.cpp
@@ -21,7 +21,7 @@
 
 /* Declarations */
 char *STATION_IP = NULL;
-static struct sockaddr_in addr_drone, addr_client;
+static struct sockaddr_in addr_drone;
 int comfd = 0; // in read mode -- receive command
 
 /* Functions */
@@ -33,7 +33,7 @@ int comfd = 0; // in read mode -- receive command
  *
  * \return the command
  */
-std::string parseCommand(char *buf) {
+void parseCommand(char *buf) {
 	char cmd[PACKET_SIZE];
 	int seq, tcmd, p1, p2, p3, p4, p5;
 	static int pseq, ptcmd = 0, pp1 = 0, pp2 = 0, pp3 = 0, pp4 = 0, pp5 = 0;
@@ -41,32 +41,32 @@ std::string parseCommand(char *buf) {
 	//AT*FTRIM=7
 	//AT*REF=78,290718208
 
-	if (!buf) return NULL;
+	if (!buf) return;
 
 	// use sscan to parse the command
 	if(sscanf(buf, "AT*FTRIM=%d", &seq) == 1) {
 		tcmd = -1;
 		if(tcmd != ptcmd)
-			return "AT*FTRIM";
+			fprintf(stderr, "%s\n", "AT*FTRIM");
 	}
 
 	else if(sscanf(buf, "AT*REF=%d, %d", &seq, &tcmd) == 2) {
 		switch(tcmd) {
 		case 290718208:
 			if(tcmd != ptcmd)
-				return "DECOLLAGE";
+				fprintf(stderr, "%s\n", "DECOLLAGE");
 
 		case 290717696:
 			if(tcmd != ptcmd)
-				return "ATTERRISSAGE";
+				fprintf(stderr, "%s\n","ATTERRISSAGE");
 			break;
 
 		case 290717952:
 			if(tcmd != ptcmd)
-				return "EMERGENCY";
+				fprintf(stderr, "%s\n","EMERGENCY");
 
 		default:
-			return "UNKNOWN";
+			fprintf(stderr, "%s\n","UNKNOWN");
 		}
 	}
 
@@ -74,32 +74,30 @@ std::string parseCommand(char *buf) {
 		if((p1 != pp1) || (p2 != pp2) || (p3 != pp3) || (p4 != pp4) || (p5 != pp5)) {
 			if(p1 || p2 || p3 || p4 || p5) {
 				if ((p1 == 1) && !p2 && (p3 < 0) && !p4 && !p5)
-					return "FORWARD";
+					fprintf(stderr, "%s\n","FORWARD");
 				else if((p1 == 1) && !p2 && (p3 > 0) && !p4 && !p5)
-					return "BACKWARD";
+					fprintf(stderr, "%s\n","BACKWARD");
 				else if((p1 == 1) && !p2 && !p3 && (p4 < 0) && !p5)
-					return "DOWN";
+					fprintf(stderr, "%s\n","DOWN");
 				else if((p1 == 1) && !p2 && !p3 && (p4 > 0) && !p5)
-					return "UP";
+					fprintf(stderr, "%s\n","UP");
 				else if((p1 == 1) && !p2 && !p3 && !p4 && (p5 < 0))
-					return "LEFT";
+					fprintf(stderr, "%s\n","LEFT");
 				else if((p1 == 1) && !p2 && !p3 && !p4 && (p5 > 0))
-					return "RIGHT";
+					fprintf(stderr, "%s\n","RIGHT");
 				else {
-					std::stringstream ss;
-					ss << "PCMD " << seq << " " << p1 << " " << p2 << " " << p3 << " " << p4 << " " << p5 ;
-					std::string s = ss.str();
-					return s;
+					fprintf(stderr,"received PCMD: %d,%d,%d,%d,%d,%d\n",seq,p1,p2,p3,p4,p5);
 				}
 			}
 
 			else {
-				return "STAY";
+				fprintf(stderr, "%s\n","STAY");
 			}
 		}
+		pp1 = p1; pp2= p2; pp3= p3; pp4= p4; pp5= p5;
 	}
 
-	return NULL;
+	ptcmd = tcmd;
 }
 
 //////////////////// Parrot channels
@@ -113,6 +111,11 @@ unsigned char commandBuffer[PACKET_SIZE+1];
  *
  */
 int main(int argc, char *argv[]) {
+	// Initialize ros for this node
+	ros::init(argc, argv, "pikopter_cmd");
+
+	ros::start();
+
 	// Instance of PikopterCmd class
 	PikopterCmd pik;
 
@@ -125,6 +128,9 @@ int main(int argc, char *argv[]) {
 	// set a timeout
 	tv.tv_sec = 0;
   	tv.tv_usec = 100000; // 100ms
+	
+	// Open the UDP port for the cmd node
+	comfd = PikopterNetwork::open_udp_socket(PORT_CMD, &addr_drone, argv[1]);
 
   	if (setsockopt(comfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
   		ROS_ERROR("%s", "Error timeout");
@@ -141,9 +147,6 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	// Initialize ros for this node
-	ros::init(argc, argv, "pikopter_cmd");
-
 	// Create a NodeHandle
 	ros::NodeHandle nodeHandle;
 	
@@ -153,8 +156,6 @@ int main(int argc, char *argv[]) {
 	// Frequency of how fast we loop
 	ros::Rate loop_rate(10);
 
-	// Open the UDP port for the cmd node
-	comfd = PikopterNetwork::open_udp_socket(PORT_CMD, &pik.addr_drone_cmd, argv[1]);
 
 	// The first argument is the IP address of the Raspberry PI
 	STATION_IP = argv[1];
@@ -168,23 +169,13 @@ int main(int argc, char *argv[]) {
 		// fprintf(stderr, "Waiting packet from %s:%d\n", inet_ntoa(addr_drone.sin_addr), ntohs(addr_drone.sin_port));
 		
 		// need to add a watchdog here
-		int ret = recvfrom(comfd, commandBuffer, PACKET_SIZE, 0, (struct sockaddr*) STATION_IP, (socklen_t*) sizeof(STATION_IP));
+		int ret = recvfrom(comfd, commandBuffer, PACKET_SIZE, 0, (struct sockaddr*) &addr_drone, &len);
 		
 		// if we receive something...
 		if(ret > 0) {			
 			// Get command
-			std::string command = parseCommand((char *) commandBuffer);
-			
-			// Building message log
-			std::stringstream ssLog;
-			ssLog << "RECEIVED COMMAND: [" << command << "]";
-			std::string sLog = ssLog.str();
-			ROS_INFO("%s", sLog.c_str());
-			
-			// Building String message to publish to the topic pikopter_mavlink
-			std_msgs::String msg;
-			msg.data = sLog;
-			cmd_pub.publish(msg);
+			printf("%s\n", commandBuffer);
+			parseCommand((char *) commandBuffer);
 		}
 
 		// if nothing is received
@@ -201,11 +192,15 @@ int main(int argc, char *argv[]) {
 			sendto(comfd, "\0", 1, 0, (struct sockaddr*) &addr_drone, sizeof(addr_drone));
 		}
 
+		ros::spinOnce();
+
 		// Pause in loop with the given value defined in ros::rate
 		loop_rate.sleep();
 	}
 
 	// close UDP socket
 	close(comfd);
+
+	ros::shutdown();
 	return 0;
 }
