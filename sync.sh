f4aa62cf697bd1974cb4028012f93d11a9352000
Andriamilanto Tompoariniaina <contact@napi.fr>
2016-02-24 15:25:21 +0100

    chores(sync.sh): Updated the includes and excludes management

--- sync.sh
+++ sync.sh
@@ -1,13 +1,7 @@
 rsync -e "ssh -i $PIKEYPATH" \
 --verbose --links --delete --compress --recursive --times --human-readable --no-perms \
---exclude=".git" \
---exclude=".gitignore" \
---exclude="sync.sh" \
---exclude="build" \
---exclude="devel" \
---exclude="src/CMakeFiles" \
---include="devel/setup.bash" \
---include="devel/setup.sh" \
---include="devel/setup.zsh" \
-catkin_workspace \
+--exclude="CMakeFiles" \
+--exclude="CMakeLists.txt" \
+catkin_workspace/devel/setup.* \
+catkin_workspace/src/pikopter \
 pi@10.5.5.1:/home/pi/ros_drone
